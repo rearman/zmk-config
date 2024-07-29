28d125e0a6d854e077d765d2f8e08b2ff2a4f796
rearman <rearman@r717.net>
2024-07-29 11:05:49 -0400

    Better C macros

--- config/42-keys.h
+++ config/42-keys.h
@@ -8,14 +8,6 @@
 #define LT4 4
 #define LT5 5
 
-/* Right Top Row */
-#define RT0 6
-#define RT1 7
-#define RT2 8
-#define RT3 9
-#define RT4 10
-#define RT5 11
-
 /* Left Middle Row */
 #define LM0 12
 #define LM1 13
@@ -23,14 +15,6 @@
 #define LM3 15
 #define LM4 16
 #define LM5 17
-
-/* Right Middle Row */
-#define RM0 18
-#define RM1 19
-#define RM2 20
-#define RM3 21
-#define RM4 22
-#define RM5 23
 
 /* Left Bottom Row */
 #define LB0 24
@@ -40,6 +24,24 @@
 #define LB4 28
 #define LB5 29
 
+#define LEFT_FINGERS LT0 LT1 LT2 LT3 LT4 LT5 LM0 LM1 LM2 LM3 LM4 LM5 LB0 LB1 LB2 LB3 LB4 LB5
+
+/* Right Top Row */
+#define RT0 6
+#define RT1 7
+#define RT2 8
+#define RT3 9
+#define RT4 10
+#define RT5 11
+
+/* Right Middle Row */
+#define RM0 18
+#define RM1 19
+#define RM2 20
+#define RM3 21
+#define RM4 22
+#define RM5 23
+
 /* Right Bottom Row */
 #define RB0 30
 #define RB1 31
@@ -48,20 +50,21 @@
 #define RB4 34
 #define RB5 35
 
+#define RIGHT_FINGERS RT0 RT1 RT2 RT3 RT4 RT5 RM0 RM1 RM2 RM3 RM4 RM5 RB0 RB1 RB2 RB3 RB4 RB5
+
 /* Left Thumbs Row */
 #define LH0 36
 #define LH1 37
 #define LH2 38
+
+#define LEFT_THUMBS LH0 LH1 LH2
 
 /* Right Thumbs Row */
 #define RH0 39
 #define RH1 40
 #define RH2 41
 
-#define LEFT_FINGERS LT0 LT1 LT2 LT3 LT4 LT5 LM0 LM1 LM2 LM3 LM4 LM5 LB0 LB1 LB2 LB3 LB4 LB5
-#define RIGHT_FINGERS RT0 RT1 RT2 RT3 RT4 RT5 RM0 RM1 RM2 RM3 RM4 RM5 RB0 RB1 RB2 RB3 RB4 RB5
-#define LEFT_THUMBS LH0 LH1 LH2
 #define RIGHT_THUMBS RH0 RH1 RH2
 
-#define LEFT_SIDE LEFT_FINGERS LEFT_THUMBS RIGHT_THUMBS
-#define RIGHT_SIDE RIGHT_FINGERS LEFT_THUMBS RIGHT_THUMBS
+#define LEFT_SIDE <LEFT_FINGERS LEFT_THUMBS RIGHT_THUMBS>
+#define RIGHT_SIDE <RIGHT_FINGERS LEFT_THUMBS RIGHT_THUMBS>
